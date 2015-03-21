2fbedf546d033396b6b7df8f1ba001593b0aca60
Alexander Bennell <alexanderbennell@Alexanders-MacBook-Pro.local>
2015-03-21 17:03:16 +0000

    Initial Commit

---
+++ www/js/index.js
@@ -0,0 +1,29 @@
+var app = {
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
+
+    renderHomeView: function () {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+
+    },
+
+    initialize: function () {
+        var self = this;
+
+        this.store = new MemoryStore(function () {
+            self.renderHomeView();
+        });
+
+
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+
+    }
+};
