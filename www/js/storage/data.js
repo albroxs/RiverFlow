24bbd03da018257345e69826a1e73b0ab1fe416b
Alexander Bennell <alexanderbennell@Alexanders-MacBook-Pro.local>
2015-03-21 22:55:27 +0000

    now gets all rivers and stores in the local db.

--- www/js/storage/data.js
+++ www/js/storage/data.js
@@ -1,11 +1,12 @@
 // Wait for Cordova to load
 document.addEventListener("deviceready", onDeviceReady, false);
 
+
 // Cordova is ready
 function onDeviceReady() {
 
-   var url = 'http://api.rainchasers.com/v1/river?ts=1357504926';
 
+    var url = 'http://api.rainchasers.com/v1/river?ts=1357504926';
     var db = window.sqlitePlugin.openDatabase({name: "DB"});
 
     db.transaction(function (tx) {
@@ -13,19 +14,20 @@ function onDeviceReady() {
         tx.executeSql('CREATE TABLE IF NOT EXISTS RIVERS (uuid PRIMARY KEY, url TEXT, riverName TEXT, riverSection TEXT, km TEXT, gradeText TEXT, description TEXT, directions TEXT, putinLat TEXT, putinLng TEXT, takeOutLat TEXT, takeOUTLng TEXT)');
 
     });
+
+    populateDB(url)
+
+}
+
+function populateDB(url) {
+
+
     $.getJSON(url, function (result) {
 
         var nextURL = result.meta.link.next;
 
-        console.log(nextURL);
-        console.log(result);
-
-
-
         $.each(result.data, function (i, field) {
-
-            console.log(result.data[i].uuid);
-
+            console.log(result.data[i]);
             var uuid = result.data[i].uuid;
             var url = result.data[i].url;
             var riverName = result.data[i].river;
@@ -39,14 +41,16 @@ function onDeviceReady() {
             var takeOutLat = result.data[i].position[1].lat;
             var takeOutLng = result.data[i].position[1].lat;
 
-            db.transaction(function (tx)
-            {
+            db.transaction(function (tx) {
                 tx.executeSql("INSERT INTO RIVERS (uuid, url, riverName, riverSection, km, description, directions, putinLat, putinLng, takeOutLat, takeOutLng) " +
-                "VALUES ("+ uuid + "," + url + "," + riverName + "," + riverSection + "," + km + "," + gradeText + "," + description + "," +
+                "VALUES (" + uuid + "," + url + "," + riverName + "," + riverSection + "," + km + "," + gradeText + "," + description + "," +
                 directions + "," + putinLat + "," + putinLng + "," + takeOutLat + "," + takeOutLng + ")");
             });
 
         });
-    });
 
+        if (nextURL != NULL) {
+            populateDB(nextURL);
+        }
+    });
 }
