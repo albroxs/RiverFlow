528f6d7a890950ede0d577ca960580f7fc17363f
Alexander Bennell <alexanderbennell@Alexanders-MacBook-Pro.local>
2015-03-21 22:22:38 +0000

    db now populates with API response

--- www/js/storage/data.js
+++ www/js/storage/data.js
@@ -5,19 +5,38 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     var db = window.sqlitePlugin.openDatabase({name: "DB"});
 
-    url = 'http://api.rainchasers.com/v1/river?ts=1357504926';
-    $.getJSON(url, function(result){
-        $.each(result, function(i, field){
-          var print = field[i].uuid;
+    db.transaction(function (tx) {
+        tx.executeSql('DROP TABLE IF EXISTS RIVERS');
+        tx.executeSql('CREATE TABLE IF NOT EXISTS RIVERS (uuid PRIMARY KEY, url TEXT, riverName TEXT, riverSection TEXT, km TEXT, gradeText TEXT, description TEXT, directions TEXT, putinLat TEXT, putinLng TEXT, takeOutLat TEXT, takeOUTLng TEXT)');
 
-            console.log(print);
+    });
+    $.getJSON(url, function (result) {
+
+        var nextURL = result.meta.link.next;
+
+        $.each(data, function (i, field) {
+
+            var uuid = result.data[i].uuid;
+            var url = result.data[i].url;
+            var riverName = result.data[i].river;
+            var riverSection = result.data[i].section;
+            var km = result.data[i].km;
+            var gradeText = result.data[i].grade.text;
+            var description = result.data[i].desc;
+            var directions = result.data[i].directions;
+            var putinLat = result.data[i].position[0].lat;
+            var putinLng = result.data[i].position[0].lng;
+            var takeOutLat = result.data[i].position[1].lat;
+            var takeOutLng = result.data[i].position[1].lat;
+
+            db.transaction(function (tx)
+            {
+                tx.executeSql("INSERT INTO RIVERS (uuid, url, riverName, riverSection, km, description, directions, putinLat, putinLng, takeOutLat, takeOutLng) " +
+                "VALUES ("+ uuid + "," + url + "," + riverName + "," + riverSection + "," + km + "," + gradeText + "," + description + "," +
+                directions + "," + putinLat + "," + putinLng + "," + takeOutLat + "," + takeOutLng + ")");
+            });
+
         });
     });
 
-    db.transaction(function(tx) {
-        tx.executeSql('DROP TABLE IF EXISTS test_table');
-        tx.executeSql('CREATE TABLE IF NOT EXISTS test_table (id integer primary key, data text, data_num integer)');
-
-
-        });
 }
