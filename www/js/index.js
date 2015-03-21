68f6f4a1d61cddd5d9abb3942b377d00f7776c6d
Alexander Bennell <alexanderbennell@Alexanders-MacBook-Pro.local>
2015-03-21 20:01:59 +0000

    Added sql test

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,23 @@
 var app = {
 
-    showAlert: function (message, title) {
-        if (navigator.notification) {
-            navigator.notification.alert(message, null, title, 'OK');
-        } else {
-            alert(title ? (title + ": " + message) : message);
-        }
+    findByName: function() {
+        console.log('findByName');
+        //this.store.findByName($('.search-key').val(), function(rivers) {
+        //    var l = rivers.length;
+        //    var e;
+        //    $('.employee-list').empty();
+        //    for (var i=0; i<l; i++) {
+        //        e = rivers[i];
+        //        $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+        //    }
+        //});
     },
 
-    renderHomeView: function () {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-
-    },
-
-    initialize: function () {
-        var self = this;
-
-        this.store = new MemoryStore(function () {
-            self.renderHomeView();
-        });
-
-
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-
+    initialize: function() {
+        //this.store = new MemoryStore();
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
+
 };
+
+app.initialize();
